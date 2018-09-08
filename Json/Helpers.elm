50f0fa43a2b7bbb9d395ac937786b82519a287b2
Matt McHenry <github@matt.mchenryfamily.org>
2018-09-08 10:22:05 -0400

    result of running elm-upgrade

--- Json/Helpers.elm
+++ Json/Helpers.elm
@@ -1,28 +1,14 @@
-module Json.Helpers
-    exposing
-        ( ObjectEncoding
-        , encodeValue
-        , encodeObject
-        , decodeSumObjectWithSingleField
-        , decodeSumTwoElemArray
-        , decodeSumTaggedObject
-        , encodeSumObjectWithSingleField
-        , encodeSumTwoElementArray
-        , encodeSumTaggedObject
-        , encodeSumUntagged
-        , decodeMap
-        , encodeMap
-        , jsonEncDict
-        , jsonDecDict
-        , encodeSet
-        , decodeSet
-        , decodeSumNullaries
-        , decodeSumUnaries
-        , decodeSumNullaryOrSingleField
-        , maybeEncode
-        , (>>=)
-        , (:=)
-        )
+module Json.Helpers exposing
+    ( ObjectEncoding, encodeObject, encodeValue
+    , decodeSumObjectWithSingleField, encodeSumObjectWithSingleField
+    , decodeSumTwoElemArray, encodeSumTwoElementArray
+    , decodeSumTaggedObject, encodeSumTaggedObject
+    , decodeSumUnaries
+    , decodeSumNullaries
+    , decodeSumNullaryOrSingleField
+    , decodeMap, encodeMap, jsonEncDict, jsonDecDict, encodeSet, decodeSet, maybeEncode, (:=), encodeSumUntagged
+    , (>>=)
+    )
 
 {-| This module exposes helper functions for encoding sum types and maps. It was designed
 with an eye for compatibility with the `aeson` library from the Haskell world, which explains
@@ -31,16 +17,21 @@ why the various functions have such peculiar names.
 If you require Haskell interop, please take a look at the [elm-bridge](https://hackage.haskell.org/package/elm-bridge) package that
 will make it easy to derive the Elm code alongside the Haskell one.
 
+
 # The ObjectEncoding type
+
 @docs ObjectEncoding, encodeObject, encodeValue
+
 
 # Encoding schemes
 
 The following Elm type will be used as an example for the different encoding schemes.
 
-    type Foo = Bar Int
-             | Baz { a: Int , b: Int }
-             | Qux Int Int
+    type Foo
+        = Bar Int
+        | Baz { a : Int, b : Int }
+        | Qux Int Int
+
 
 ## ObjectWithSingleField
 
@@ -48,7 +39,10 @@ The following Elm type will be used as an example for the different encoding sch
     -- {"Baz":{"a":4,"b":8}}
     -- {"Qux":[98,42]}
 
+
+
 @docs decodeSumObjectWithSingleField, encodeSumObjectWithSingleField
+
 
 ## TwoElemArray
 
@@ -56,7 +50,10 @@ The following Elm type will be used as an example for the different encoding sch
     -- ["Baz",{"a":4,"b":8}]
     -- ["Qux",[98,42]]
 
+
+
 @docs decodeSumTwoElemArray, encodeSumTwoElementArray
+
 
 ## TaggedObject
 
@@ -64,7 +61,10 @@ The following Elm type will be used as an example for the different encoding sch
     -- {"tag":"Baz","a":4,"b":8}
     -- ["tag":"Qux","content":[98,42]}
 
+
+
 @docs decodeSumTaggedObject, encodeSumTaggedObject
+
 
 ## Nullary sum types
 
@@ -73,6 +73,7 @@ The following Elm type will be used as an example for the different encoding sch
 
 @docs decodeSumNullaryOrSingleField
 
+
 # Containers helpers
 
 @docs decodeMap, encodeMap, jsonEncDict, jsonDecDict, encodeSet, decodeSet, maybeEncode, (>>=), (:=), encodeSumUntagged
@@ -80,9 +81,9 @@ The following Elm type will be used as an example for the different encoding sch
 -}
 
 import Dict exposing (Dict)
-import Set exposing (Set)
+import Json.Decode exposing (Value, field)
 import Json.Encode
-import Json.Decode exposing (field, Value)
+import Set exposing (Set)
 
 
 {-| This is an opaque type that is to be used to give hints when using the `TaggedObject` encoding.
@@ -138,19 +139,28 @@ resmapM f lst =
             f x |> Result.andThen (\nx -> resmapM f xs |> Result.andThen (\nxs -> Ok (nx :: nxs)))
 
 
+
 -- polyfill
+
+
 tuple2 : (a -> b -> value) -> Json.Decode.Decoder a -> Json.Decode.Decoder b -> Json.Decode.Decoder value
 tuple2 abv da db =
     Json.Decode.map2 abv (Json.Decode.index 0 da) (Json.Decode.index 1 db)
 
 
+
 -- polyfill from https://groups.google.com/d/msg/elm-dev/Ctl_kSKJuYc/7nCM8XETBwAJ
+
+
 customDecoder decoder toResult =
     Json.Decode.andThen
         (\a ->
             case toResult a of
-                Ok b -> Json.Decode.succeed b
-                Err err -> Json.Decode.fail err
+                Ok b ->
+                    Json.Decode.succeed b
+
+                Err err ->
+                    Json.Decode.fail err
         )
         decoder
 
@@ -180,7 +190,8 @@ are decoders for each case.
 -}
 decodeSumTwoElemArray : String -> Dict String (Json.Decode.Decoder a) -> Json.Decode.Decoder a
 decodeSumTwoElemArray name mapping =
-    customDecoder (tuple2 (,) Json.Decode.string Json.Decode.value) (\( key, value ) -> decodeSumFinal name key value mapping)
+    customDecoder (tuple2 (\a b -> ( a, b )) Json.Decode.string Json.Decode.value) (\( key, value ) -> decodeSumFinal name key value mapping)
+
 
 {-| Decode objects encoded using the `TaggedObject` scheme.
 The first argument is the human readable name of the type of data, and will be used in error messages.
@@ -189,20 +200,22 @@ are decoders for each case.
 
 Compared to the other functions, it expects a set of `String`s. This sets lists all the constructor tags that have an object content,
 such as the `Baz` constructor in the example.
+
 -}
 decodeSumTaggedObject : String -> String -> String -> Dict String (Json.Decode.Decoder a) -> Set String -> Json.Decode.Decoder a
 decodeSumTaggedObject name fieldname contentname mapping objectKeys =
-    (field fieldname Json.Decode.string)
+    field fieldname Json.Decode.string
         |> Json.Decode.andThen
             (\key ->
                 let
                     decoder =
                         if Set.member key objectKeys then
                             Json.Decode.value
+
                         else
                             field contentname Json.Decode.value
                 in
-                    customDecoder decoder (\value -> decodeSumFinal name key value mapping)
+                customDecoder decoder (\value -> decodeSumFinal name key value mapping)
             )
 
 
@@ -215,6 +228,7 @@ decodeSumFinal name key value mapping =
         Just dec ->
             Json.Decode.decodeValue dec value
 
+
 {-| Encode objects using the `WithSingleField` scheme.
 The first argument is a function that, for each possible value `a`, must return a `String` tag
 describing it along with an `ObjectEncoding`.
@@ -225,7 +239,7 @@ encodeSumObjectWithSingleField mkkeyval v =
         ( key, val ) =
             mkkeyval v
     in
-        Json.Encode.object [ ( key, oeValue val ) ]
+    Json.Encode.object [ ( key, oeValue val ) ]
 
 
 {-| Encode objects using the `TwoElementArray` scheme.
@@ -238,7 +252,7 @@ encodeSumTwoElementArray mkkeyval v =
         ( key, val ) =
             mkkeyval v
     in
-        Json.Encode.list [ Json.Encode.string key, oeValue val ]
+    Json.Encode.list [ Json.Encode.string key, oeValue val ]
 
 
 {-| Encode objects using the `TaggedObject` scheme.
@@ -254,19 +268,24 @@ encodeSumTaggedObject fieldname contentname mkkeyval v =
         kp =
             ( fieldname, Json.Encode.string key )
     in
-        case eval of
-            EValue val ->
-                Json.Encode.object [ kp, ( contentname, val ) ]
+    case eval of
+        EValue val ->
+            Json.Encode.object [ kp, ( contentname, val ) ]
 
-            EObject obj ->
-                Json.Encode.object (kp :: obj)
+        EObject obj ->
+            Json.Encode.object (kp :: obj)
 
-{-| Encode objects using the `Untagged` scheme. -}
+
+{-| Encode objects using the `Untagged` scheme.
+-}
 encodeSumUntagged : (a -> ( String, ObjectEncoding )) -> a -> Value
-encodeSumUntagged mkkeyval v
-    = case Tuple.second (mkkeyval v) of
-        EValue val -> val
-        EObject o -> Json.Encode.object o
+encodeSumUntagged mkkeyval v =
+    case Tuple.second (mkkeyval v) of
+        EValue val ->
+            val
+
+        EObject o ->
+            Json.Encode.object o
 
 
 {-| Helper for decoding enum-like sum types
@@ -284,19 +303,29 @@ decodeSumNullaries typename mapping =
                         Json.Decode.succeed x
             )
 
+
 {-| This function is deprecated, use `decodeSumNullaries` (it is the same, only with an appropriate name)
 -}
 decodeSumUnaries : String -> Dict String a -> Json.Decode.Decoder a
-decodeSumUnaries = decodeSumNullaries
+decodeSumUnaries =
+    decodeSumNullaries
 
-{-| A convenience function to decode objects that are represented as a sum type containing only nullary or unary constructors -}
+
+{-| A convenience function to decode objects that are represented as a sum type containing only nullary or unary constructors
+-}
+
+
 
 -- Code copied from radix, see https://github.com/bartavelle/json-helpers/issues/4
+
+
 decodeSumNullaryOrSingleField : String -> Dict String a -> Dict String (Json.Decode.Decoder a) -> Json.Decode.Decoder a
-decodeSumNullaryOrSingleField name nullary singlefield = Json.Decode.oneOf
+decodeSumNullaryOrSingleField name nullary singlefield =
+    Json.Decode.oneOf
         [ decodeSumUnaries name nullary
         , decodeSumObjectWithSingleField name singlefield
         ]
+
 
 {-| Helper function for decoding map-like objects. It takes a decoder for the key type and a decoder for the value type.
 -}
@@ -309,7 +338,7 @@ decodeMap decKey decVal =
         decodeKey ( k, v ) =
             Result.map (\nk -> ( nk, v )) (Json.Decode.decodeString decKey k)
     in
-        Json.Decode.map Dict.fromList (customDecoder (Json.Decode.keyValuePairs decVal) decodeKeys)
+    Json.Decode.map Dict.fromList (customDecoder (Json.Decode.keyValuePairs decVal) decodeKeys)
 
 
 {-| Helper function for encoding map-like objects. It takes an encoder for the key type and an encoder for the value type
@@ -325,7 +354,7 @@ encodeMap encKey encVal =
                 Ok s ->
                     s
     in
-        Json.Encode.object << List.map (\( k, v ) -> ( encKey_ k, encVal v )) << Dict.toList
+    Json.Encode.object << List.map (\( k, v ) -> ( encKey_ k, encVal v )) << Dict.toList
 
 
 {-| An alias to `encodeMap` that is compatible with the naming convention from `elm-bridge`
@@ -355,10 +384,16 @@ decodeSet : Json.Decode.Decoder comparable -> Json.Decode.Decoder (Set comparabl
 decodeSet d =
     Json.Decode.map Set.fromList (Json.Decode.list d)
 
-{-| The bind operator, which works like the old `andThen -}
-(>>=) : Json.Decode.Decoder a -> (a -> Json.Decode.Decoder b) -> Json.Decode.Decoder b
-(>>=) = flip Json.Decode.andThen
 
-{-| This is the old operator from Elm 0.17 -}
+{-| The bind operator, which works like the old \`andThen
+-}
+(>>=) : Json.Decode.Decoder a -> (a -> Json.Decode.Decoder b) -> Json.Decode.Decoder b
+(>>=) =
+    \b a -> Json.Decode.andThen a b
+
+
+{-| This is the old operator from Elm 0.17
+-}
 (:=) : String -> Json.Decode.Decoder a -> Json.Decode.Decoder a
-(:=) = Json.Decode.field
+(:=) =
+    Json.Decode.field
